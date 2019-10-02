318856fb53433e8d4e51bbfea824289c7db38919
Travis CI User <travis@example.org>
2019-10-02 03:31:26 +0000

    Documentation (Wed Oct  2 03:31:26 UTC 2019)

--- versions.js
+++ versions.js
@@ -1,1 +1,1 @@
-var versions = ['develop', 'r/7.x', 'r/6.x', 'r/5.x', 'r/4.x', 'r/3.x', 'r/2.3.x', 'r/2.2.x', 'r/2.1.x', 'r/2.0.x'];
+var versions = ['develop', 'r/8.x', 'r/7.x', 'r/6.x', 'r/5.x', 'r/4.x', 'r/3.x', 'r/2.3.x', 'r/2.2.x', 'r/2.1.x', 'r/2.0.x'];
